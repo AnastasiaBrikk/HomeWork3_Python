8d1f24be236e1fa9b86ef489b68f7fa0b27ce072
Anastasia <nastya.proskuryakova.98@mail.ru>
2023-01-11 19:15:43 +0300

    there is a mistake, need to find and fix it

--- 3-2.py
+++ 3-2.py
@@ -3,13 +3,25 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+n = int(input('Input a length of random list: '))
 
-list1 = [2, 3, 4, 5, 6]
-list2 = [2, 3, 5, 6]
-def proiz(list):
+
+def FillRandomList(n):
+    list1 = []
+    from random import randint
+    for i in range(n):
+        list1.append(randint(0, 9))
+    return list1
+
+
+def proiz(list):      # работает через раз. Непонятно почему
     last_index = len(list)-1
     for i in list:
         if list.index(i) < len(list)/2:
-            print(i*(list[last_index]-list.index(i)))
-proiz(list1)
-proiz(list2)
+            print(i*list[(last_index-list.index(i))])
+        else:
+            break
+
+random_list = FillRandomList(n)
+print(random_list)
+proiz(random_list)
