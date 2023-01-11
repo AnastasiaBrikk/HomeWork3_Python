c7e7b48f62d1168afbacd7d046711d4b23b6bd25
Anastasia <nastya.proskuryakova.98@mail.ru>
2023-01-11 19:44:21 +0300

    Fix problem, but delete defs

--- 3-2.py
+++ 3-2.py
@@ -1,27 +1,31 @@
 # 2 Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# list = [1, 2, 3, 4, 5]
+# print(len(list)//2 + len(list)%2) # целочисленное деление на два. 5// 2 = 2 (без дробной доли 0,5)
+
+# print(len(list)/2) # остаток от деления на два. 5 % 2 = 0.5
+
 
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
 n = int(input('Input a length of random list: '))
+result_list = []
+list = []
+
+from random import randint
+for i in range(n):
+    list.append(randint(0, 9))
+print(list)
+
+i = 0
+while i < len(list)//2 + len(list)%2:
+    n = n - 1
+    a = list[i] * list[n]
+    result_list.append(a)
+    i += 1
+
+print(result_list)
 
 
-def FillRandomList(n):
-    list1 = []
-    from random import randint
-    for i in range(n):
-        list1.append(randint(0, 9))
-    return list1
 
-
-def proiz(list):      # работает через раз. Непонятно почему
-    last_index = len(list)-1
-    for i in list:
-        if list.index(i) < len(list)/2:
-            print(i*list[(last_index-list.index(i))])
-        else:
-            break
-
-random_list = FillRandomList(n)
-print(random_list)
-proiz(random_list)
